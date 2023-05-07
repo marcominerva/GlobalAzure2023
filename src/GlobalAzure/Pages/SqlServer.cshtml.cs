82481c247640f4247c08a4de3a6ab152531ac1da
Marco Minerva <marco.minerva@gmail.com>
2023-05-07 11:42:02 +0200

    Add Database

---
+++ src/GlobalAzure/Pages/SqlServer.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace GlobalAzure.Pages;
+
+public class SqlServerModel : PageModel
+{
+    private readonly IConfiguration configuration;
+
+    public string? SqlVersion { get; set; }
+
+    public IEnumerable<Person> People { get; set; } = default!;
+
+    public SqlServerModel(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public async Task OnGetAsync()
+    {
+        using var sqlConnection = new SqlConnection(configuration.GetConnectionString("SqlConnection"));
+        await sqlConnection.OpenAsync();
+
+        SqlVersion = await sqlConnection.ExecuteScalarAsync<string>("SELECT @@VERSION");
+        People = await sqlConnection.QueryAsync<Person>("SELECT * FROM People ORDER BY FirstName, LastName");
+    }
+}
+
+public record class Person(Guid Id, string FirstName, string LastName, string City);
