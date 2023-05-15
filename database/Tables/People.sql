2e08daaa7b05917260ef2c3ea58b11591be84c70
Marco Minerva <marco.minerva@gmail.com>
2023-05-15 09:23:51 +0200

    Change database project

--- database/Tables/People.sql
+++ database/Tables/People.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[People] (
     [Id]        UNIQUEIDENTIFIER NOT NULL,
     [FirstName] NVARCHAR (50)    NOT NULL,
     [LastName]  NVARCHAR (50)    NULL,
-    [City]      NVARCHAR (80)    NOT NULL
+    [City]      NVARCHAR (80)    NOT NULL,
+    [IsDeleted] BIT              NOT NULL
 );
 GO
 
@@ -12,5 +13,10 @@ GO
 
 ALTER TABLE [dbo].[People]
     ADD CONSTRAINT [DF_People_Id] DEFAULT (newid()) FOR [Id];
+GO
+
+
+ALTER TABLE [dbo].[People]
+    ADD CONSTRAINT [DF_People_IsDeleted] DEFAULT ((0)) FOR [IsDeleted];
 GO
 
